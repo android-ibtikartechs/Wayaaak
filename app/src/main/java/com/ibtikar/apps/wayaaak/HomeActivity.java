14b8c0bb72a54a12312075fab8ee08bd47fcb102
Ibtikar100 <android@ibtikartechs.com>
2018-06-20 17:37:24 +0200

    g

--- app/src/main/java/com/ibtikar/apps/wayaaak/HomeActivity.java
+++ app/src/main/java/com/ibtikar/apps/wayaaak/HomeActivity.java
@@ -2,12 +2,21 @@ package com.ibtikar.apps.wayaaak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class HomeActivity extends AppCompatActivity {
+    Spinner citiesSpin, areaSpin, catSpin;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        citiesSpin = findViewById(R.id.cities_spin);
+        areaSpin = findViewById(R.id.area_spin);
+        catSpin = findViewById(R.id.cat_spin);
+        btnSearch = findViewById(R.id.button_search);
+
     }
 }
