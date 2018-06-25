6a2e6a23e637f16ff4e16d96aa77e0e4de5d3615
Ibtikar100 <android@ibtikartechs.com>
2018-06-25 17:47:10 +0200

    g

--- app/src/main/java/com/ibtikar/apps/wayaaak/HomeActivity.java
+++ app/src/main/java/com/ibtikar/apps/wayaaak/HomeActivity.java
@@ -1,6 +1,14 @@
 package com.ibtikar.apps.wayaaak;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,8 +16,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.gson.Gson;
 import com.hosamazzam.volleysimple.VolleySimple;
 import com.ibtikar.apps.wayaaak.Adapters.SpinerAdapter;
@@ -20,13 +35,21 @@ import com.ibtikar.apps.wayaaak.Tools.WayaaakAPP;
 import com.yarolegovich.slidingrootnav.SlidingRootNav;
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
+import java.io.IOException;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
     private SlidingRootNav slidingRootNav;
     Spinner citiesSpin, areaSpin, catSpin;
     Button btnSearch;
     VolleySimple volleySimple;
     String city, area, categ;
+    LinearLayout loutLocation;
+    TextView tvCityLocation;
     Toolbar toolbar;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private Boolean mLocationPermissionsGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +60,13 @@ public class HomeActivity extends AppCompatActivity {
         catSpin = findViewById(R.id.cat_spin);
         btnSearch = findViewById(R.id.button_search);
         volleySimple = VolleySimple.getInstance(this);
+        loutLocation = findViewById(R.id.lout_location);
+        tvCityLocation = findViewById(R.id.tv_city);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         slidingRootNav = new SlidingRootNavBuilder(this)
                 .withToolbarMenuToggle(toolbar)
@@ -48,6 +75,20 @@ public class HomeActivity extends AppCompatActivity {
         SlideNavigation slideNavigation = new SlideNavigation(R.id.main_content);
         slideNavigation.initSlideMenu(HomeActivity.this, getSupportFragmentManager(), slidingRootNav);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+            } else {
+                //  init();
+                getDeviceLocation();
+            }
+        } else {
+            //  init();
+            getDeviceLocation();
+        }
 
         initCities(64);
         initCatSpin();
@@ -181,6 +222,106 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    //  init();
+                    getDeviceLocation();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    //getbannerAdWithoutLocation();
+                    loutLocation.setVisibility(View.GONE);
+                    finish();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 
+
+    private void getDeviceLocation() {
+        Log.d("TAG", "getDeviceLocation: getting the current device location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+
+            com.google.android.gms.tasks.Task location = mFusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull com.google.android.gms.tasks.Task task) {
+                    if (task.isSuccessful()) {
+                        Log.d("TAG", "onComplete: location found");
+                        Location currentLocation = (Location) task.getResult();
+
+                        if (currentLocation!=null ) {
+                            //moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM, "My Location");
+
+                            Log.d("location", "onComplete: " + currentLocation.getLatitude() + " , " + currentLocation.getLongitude());
+                            LatLng latLng = new  LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                            //LatLng latLng = new  LatLng(Double.valueOf("27.067145"), Double.valueOf("27.96655106"));
+                            getCity(latLng);
+                        }
+                        else
+                            loutLocation.setVisibility(View.GONE);
+                            //getbannerAdWithoutLocation();
+                    }
+                    else {
+                        Log.d("TAG", "onComplete: current location is null");
+                        //Toast.makeText(this, "unable to find current location", Toast.LENGTH_SHORT).show();
+                        //getbannerAdWithoutLocation();
+                        loutLocation.setVisibility(View.GONE);
+                    }
+                }
+            });
+
+        } catch (SecurityException e) {
+            Log.d("TAG", "getDeviceLocation: SecurityException" + e);
+        }
+
+    }
+
+    private String getCity(LatLng latLng)
+    {
+        List<Address> addresses;
+        String adminArea = null;
+        Geocoder geocoder = new Geocoder(this);
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+
+            if (addresses.size()!=0) {
+                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                adminArea = addresses.get(0).getAdminArea();
+                Log.d("TAG", "AdminArea: " + adminArea);
+
+                Log.d("TAG", "SubAdminArea: " + addresses.get(0).getSubAdminArea());
+                Log.d("TAG", "CountryName: " + addresses.get(0).getCountryName());
+                Log.d("TAG", "FeatureName: " + addresses.get(0).getFeatureName());
+                Log.d("TAG", "Locality: " + addresses.get(0).getLocality());
+                Log.d("TAG", "SubLocality: " + addresses.get(0).getSubLocality());
+                Log.d("TAG", "PostalCode: " + addresses.get(0).getPostalCode());
+                Log.d("TAG", "Premises: " + addresses.get(0).getPremises());
+                Log.d("TAG", "Thoroughfare: " + addresses.get(0).getThoroughfare());
+                Log.d("TAG", "SubThoroughfare: " + addresses.get(0).getSubThoroughfare());
+                Log.d("TAG", "Url: " + addresses.get(0).getUrl());
+
+                //getbannerAdByLocation(adminArea);
+                tvCityLocation.setText(adminArea);
+              /*  etMapAddress.setText(address);
+                Double latt = addresses.get(0).getLatitude();
+                lastLatitude = latt.toString();
+                Log.d(TAG, "run: "+"latt = "+ lastLatitude);
+                Double longitude = addresses.get(0).getLongitude();
+                lastLongitude = longitude.toString(); */
+            }
+            else
+                loutLocation.setVisibility(View.GONE);
+                //getbannerAdWithoutLocation();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return adminArea;
+    }
 
 }
